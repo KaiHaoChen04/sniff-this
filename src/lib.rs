376e9ca90f02fde4860005fa035ff76689c05e84
kai <chenkenny414@gmail.com>
2026-09-07 16:53:59 -0400

    typo fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ pub mod modules;
 
 pub use modules::capture::{list_interfaces, run_capture};
 pub use modules::frame::{format_frame, LinkLayerFrame};
-pub use modules::protocols::LinkLayerProtocol;
+pub use modules::protocol::LinkLayerProtocol;
